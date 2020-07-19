113fcd5e6835230ea7380a4d93e5e45ad54c99d8
zskyfall <zestskyfall@gmail.com>
2020-07-19 23:05:43 +0700

    build add user layout

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,10 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/add', (req, res) => {
+
+	res.render('admin/add_user');
+
+});
+
 module.exports = router;
